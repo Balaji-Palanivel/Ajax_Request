23036510e78039ff4a8cc813c4396ef29a5869a2
Balaji-Palanivel <erbalajibe09@gmail.com>
2022-07-29 17:51:26 +0530

    Loader Fixed

--- api.js
+++ api.js
@@ -1,74 +1,71 @@
 
-function fun() {
-	let x = document.getElementById("cc").value;
+
+function ajaxCall() {
+	$("#loading").show();
+    let x = document.getElementById("cc").value;
 	if (x === "") { alert("Plase give some name for search"); }
 	else {
 		let u = 'https://openlibrary.org/search/authors.json?q=';
-		let URL = u + x;
-		ajaxCall(URL);
+		let URL = u + x;		
+		$.ajax({
 
-	}
-}
-
-//-----------------------------------------------------------------------------------------------------
-
-function ajaxCall(URL) {
-	$.ajax({
-
-		url: URL,
-		type: "GET",
-		success: function (data) {
-
-			let temp = "";
-
-			temp += "<tr>";
-			temp += "<td>" + "S.No" + "</td>";
-			temp += "<td>" + "NAME" + "</td>";
-			temp += "<td>" + "TYPE" + "</td>";
-			temp += "<td>" + "BIRTH DATE" + "</td>";
-			temp += "<td>" + "WORK COUNT" + "</td>";
-			temp += "<td>" + "TOP SUBJECTS" + "</td></tr>";
-
-			for (const [key, value] of Object.entries(data)) {
-				if (key == "numFound" && value == 0) { document.getElementById("msg").innerHTML = "No Author's are available in this name "; }
-
-				else {
-					if (key == "docs") {
-
-						for (var i = 0; i < value.length; i++) {
-
-							temp += "<tr>";
-							temp += "<th>" + i + "</th>";
-							temp += "<td id =" + value[i]['name'] + " onClick=fun2(this),document.getElementById('author').style.display='block';document.getElementById('fade').style.display='block'>" + value[i]["name"] + "</td>";
-							temp += "<td>" + value[i]["type"] + "</td>";
-							if (value[i]["birth_date"] === undefined) { temp += "<td>" + "--" + "</td>"; } else { temp += "<td>" + value[i]["birth_date"] + "</td>"; }
-							temp += "<td>" + value[i]["work_count"] + "</td>";
-							if (value[i]["top_subjects"] === undefined) { temp += "<td>" + "--" + "</td>"; } else { temp += "<td>" + value[i]["top_subjects"] + "</td></tr>"; }
-
+			url: URL,
+			type: "GET",
+			success: function (data) {
+				$("#loading").hide();
+				let temp = "";
+	
+				temp += "<tr>";
+				temp += "<td>" + "S.No" + "</td>";
+				temp += "<td>" + "NAME" + "</td>";
+				temp += "<td>" + "TYPE" + "</td>";
+				temp += "<td>" + "BIRTH DATE" + "</td>";
+				temp += "<td>" + "WORK COUNT" + "</td>";
+				temp += "<td>" + "TOP SUBJECTS" + "</td></tr>";
+	
+				for (const [key, value] of Object.entries(data)) {
+					if (key == "numFound" && value == 0) { document.getElementById("msg").innerHTML = "No Author's are available in this name "; }
+	
+					else {
+						if (key == "docs") {
+	
+							for (var i = 0; i < value.length; i++) {
+	
+								temp += "<tr>";
+								temp += "<th>" + i + "</th>";
+								temp += "<td id =" + value[i]['name'] + " onClick=fun2(this),document.getElementById('author').style.display='block';document.getElementById('fade').style.display='block'>" + value[i]["name"] + "</td>";
+								temp += "<td>" + value[i]["type"] + "</td>";
+								if (value[i]["birth_date"] === undefined) { temp += "<td>" + "--" + "</td>"; } else { temp += "<td>" + value[i]["birth_date"] + "</td>"; }
+								temp += "<td>" + value[i]["work_count"] + "</td>";
+								if (value[i]["top_subjects"] === undefined) { temp += "<td>" + "--" + "</td>"; } else { temp += "<td>" + value[i]["top_subjects"] + "</td></tr>"; }
+	
+							}
+	
 						}
-
 					}
+	
 				}
-
+	
+				document.getElementById("data").innerHTML = temp;
+	
+			},
+	
+	
+			error: function (error) {
+				console.log(`Error ${error}`);
+				$("#loading").hide();
 			}
-
-			document.getElementById("data").innerHTML = temp;
-
-		},
-
-
-		error: function (error) {
-			console.log(`Error ${error}`);
-		}
-	});
-}
+		});
+	
+	}
+	}
 
 
 
 
 //--------------------------------------------------------------------------------------------------------
 function fun2(a) {
-	myfunction();
+	
 	let u = 'https://openlibrary.org/search/authors.json?q=';
 	let URL = u + a.innerText;
 	ajaxCall1(URL);
@@ -113,13 +110,7 @@ function fun2(a) {
 	}
 
 }
-function myfunction() {
-	var element = document.getElementById("loading");
-	if (document.getElementById("data").rows.length == 0) {
-		element.style.display = "none";		
-	}
 
-}
 
 function closeProfile() {
 	
@@ -130,3 +121,4 @@ function closeProfile() {
 
 //--------------------------------------------------------------------------------------------------------------------
 
+    
